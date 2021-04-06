3be4b98f09506911c614dfd2c7e22e86d8e13560
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-04-06 23:53:47 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,3 @@
-  
 <?php
 
     date_default_timezone_set("Asia/kolkata");
@@ -11,35 +10,32 @@
     $id = $update["message"]["from"]["id"];
     $username = $update["message"]["from"]["username"];
     $firstname = $update["message"]["from"]["first_name"];
+    $start_msg = $_ENV['START_MSG']; 
 
-    $start_msg = $_ENV['START_MSG'];
-    //Start message
     if($message == "/start"){
-        send_message($chat_id,$message_id, "Hey $firstname  \nUse ```!bin xxxxxx``` To check Bins  \n $start_msg");
+        send_message($chat_id,$message_id, "***Hey $firstname \nUse !bin xxxxxx to Check BIN \n$start_msg***");
     }
 
-
-
 //Bin Lookup
-   if(strpos($message, "!bin") === 0){
-        $bin = substr($message, 5);
-   $curl = curl_init();
-   curl_setopt_array($curl, [
-	CURLOPT_URL => "https://bins-su-api.vercel.app/api/".$bin,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-		"accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
-		"sec-fetch-dest: document",
-		"sec-fetch-site: none",
-        "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
-	],
+if(strpos($message, "!bin") === 0){
+    $bin = substr($message, 5);
+$curl = curl_init();
+curl_setopt_array($curl, [
+CURLOPT_URL => "https://bins-su-api.vercel.app/api/".$bin,
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_FOLLOWLOCATION => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "GET",
+CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+],
 ]);
 
 $result = curl_exec($curl);
@@ -50,28 +46,31 @@ $country = $data['data']['country'];
 $brand = $data['data']['vendor'];
 $level = $data['data']['level'];
 $type = $data['data']['type'];
-$result = $data['result'];
-  if ($result == true) {
-    send_message($chat_id,$message_id, "***
-✅ Valid BIN
-Bin: $bin
-Brand: $brand
-Level : $level
-Bank: $bank
-Country: $country
-Type:$type
-Checked By @$username ***");
+$result1 = $data['result'];
+
+  if ($result1 == true) {
+        send_message($chat_id,$message_id, "***
+        ✅ Valid BIN
+        Bin: $bin
+        Brand: $brand
+        Level : $level
+        Bank: $bank
+        Country: $country
+        Type:$type
+        Checked By @$username ***");
     }
 else {
     send_message($chat_id,$message_id, "***Enter Valid BIN***");
 }
    }
     
-//Send Messages with Markdown (Global)
- function send_message($chat_id,$message_id, $message){
+
+      function send_message($chat_id,$message_id, $message){
         $text = urlencode($message);
         $apiToken = $_ENV['API_TOKEN'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
     }
-    
+
+
 ?>
+
